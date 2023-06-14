7759c66dd3357e5c5d8a30dc4f76fe1698c07b7c
Syrnik Konrad <syrnikkonrad@gmail.com>
2023-06-14 22:49:53 +0200

    Update api.py file

--- app/api/api.py
+++ app/api/api.py
@@ -3,7 +3,5 @@
 
 
 # TODO
-# 2. Endpoint for changing the ticket status as well as setting up resolution information/guidence for a specific ticket (/ticket/<ticket_id>)
 # 3. Endpoint for setting up and patching information about billingData
-# 4. Endpoint for patching technicalData
 # 5. Endpoint for setting up and patching information about crmData
