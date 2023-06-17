61a9882a32d920e42cccada12d42399fe1b8c493
Dziomek <dziomek@student.agh.edu.pl>
2023-06-17 22:02:05 +0200

    User filtering

--- app/api/users.py
+++ app/api/users.py
@@ -187,11 +187,18 @@ def delete_user_technical_data(user_id: int, product_id: str):
     except Exception as e:
         return jsonify({'message': 'An error occurred'}), 500
 
-@bp.route('/users', methods=['GET'])
+@bp.route('/all_users', methods=['GET'])
 def get_all_users():
     try:
         users = list(context_db.db.contexts.find({}, {'_id': 0}))
         return users, 200
+    except ValueError:
+        return jsonify('An error occured'), 400
 
+@bp.route('/specified_users', methods=['GET'])
+def get_specified_users(query):
+    try:
+        users = list(context_db.db.contexts.find(query, {'_id': 0}))
+        return users, 200
     except ValueError:
         return jsonify('An error occured'), 400
--- app/forms/admin_context_form.py
+++ app/forms/admin_context_form.py
@@ -4,10 +4,16 @@ from wtforms.validators import InputRequired, Length
 
 
 class AdminContextForm(FlaskForm):
-    name = StringField('Name', validators=[InputRequired(), Length(max=80)])
-    surname = StringField('Surname', validators=[InputRequired(), Length(max=80)])
-    email = StringField('Email', validators=[InputRequired(), Length(max=80)])
-    country = SelectField('Country', validators=[InputRequired()], choices=[])
-    city = SelectField('City', validators=[InputRequired()], choices=[])
-    router = SelectField('Router', validators=[InputRequired()], choices=[])
-    status = SelectField('Status', validators=[InputRequired()], choices=[])
+    user_id = StringField('ID', validators=[Length(max=80)])
+    name = StringField('Name', validators=[Length(max=80)])
+    surname = StringField('Surname', validators=[Length(max=80)])
+    email = StringField('Email', validators=[Length(max=80)])
+    age = StringField('Age', validators=[Length(max=80)])
+    gender = SelectField('Gender', choices=[])
+    state = SelectField('State', choices=[])
+    city = SelectField('City', choices=[])
+    router_brand = SelectField('Router brand', choices=[])
+    router_model = SelectField('Router model', choices=[])
+    mobile_brand = SelectField('Mobile brand', choices=[])
+    mobile_model = SelectField('Mobile model', choices=[])
+    subscription_type = SelectField('Subscription type', choices=[])
